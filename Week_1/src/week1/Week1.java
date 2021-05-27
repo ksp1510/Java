cfe591f73ba4843f6acc59a3b4f7424c130c24af
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2021-05-28 00:13:36 +0530

    new java file added

---
+++ Week_1/src/week1/Week1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package week1;
+
+/**
+ * @author ASUS
+ *
+ */
+public class Week1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
